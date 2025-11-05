aa3ceadde0c24a22678d434cc2d8faf9fb4b2aeb
حسين <hussein.66633@gmail.com>
2025-11-05 17:20:55 +0300

    Modify home message and change port to 8080
    Updated home response message and changed server port.

--- keep_alive.py
+++ keep_alive.py
@@ -1,28 +1,15 @@
 from flask import Flask
 from threading import Thread
-import requests, time
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive"
+    return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=3000)
-
-def ping():
-    url = "https://38b8afbf-0907-4152-a773-69dd44ae8158-00-2j3r8ebu6wghh.janeway.replit.dev/"
-    while True:
-        try:
-            requests.get(url)
-            print("Ping sent ✅")
-        except Exception as e:
-            print("Ping error ❌", e)
-        time.sleep(200)  # كل 200 ثانية
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t1 = Thread(target=run)
-    t2 = Thread(target=ping)
-    t1.start()
-    t2.start()
+    t = Thread(target=run)
+    t.start()
